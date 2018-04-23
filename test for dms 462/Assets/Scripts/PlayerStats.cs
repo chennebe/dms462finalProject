4465b96a240584faffa4ccddb4b365ad869380a0
eykim5 <eykim5@buffalo.edu>
2018-04-23 12:34:26 -0400

    aSdsasc

---
+++ test for dms 462/Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class PlayerScripts
+{
+
+    private static int ATK, DEF, SPE, HP;
+
+    public static int Attack
+    {
+        get
+        {
+            return ATK;
+        }
+        set
+        {
+            ATK = value;
+        }
+    }
+
+    public static int Defense
+    {
+        get
+        {
+            return DEF;
+        }
+        set
+        {
+            DEF = value;
+        }
+    }
+
+    public static int Speed
+    {
+        get
+        {
+            return SPE;
+        }
+        set
+        {
+            SPE = value;
+        }
+    }
+
+    public static int Health
+    {
+        get
+        {
+            return HP;
+        }
+        set
+        {
+            HP = value;
+        }
+    }
+}
